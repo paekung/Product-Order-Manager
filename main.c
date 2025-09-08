25d5b9a0a44a9899f5d334253a3e96f337a10b1b
paekung <supakit.3t@gmail.com>
2025-09-08 17:34:10 +0700

    add function - load_csv

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    
+void load_csv(const char *filename);
+
+int main(){
+    load_csv("products.csv");
+    return 0;
+}
+
+void load_csv(const char *filename){
+    FILE *fp;
+    char line[1024];
+
+    fp = fopen(filename, "r");
+
+    while (fgets(line, sizeof(line), fp)){
+        char *token = strtok(line, ",");
+        while (token){
+            printf("%s ", token);
+            token = strtok(NULL, ",");
+        }
+        printf("\n");
+    }
+
+    fclose(fp);
 }
