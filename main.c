90aa72ecf4662fee0d212d5c046d7ba15e61c490
paekung <supakit.3t@gmail.com>
2025-09-11 09:57:11 +0700

    rework load_csv function

--- main.c
+++ main.c
@@ -8,20 +8,28 @@ int main(){
     return 0;
 }
 
+
 void load_csv(const char *filename){
     FILE *fp;
     char line[1024];
 
     fp = fopen(filename, "r");
 
-    while (fgets(line, sizeof(line), fp)){
+    while(fgets(line, sizeof(line), fp)){
+        // printf("%s",line);
+
+        line[strcspn(line, "\r\n")] = '\0';
+
         char *token = strtok(line, ",");
+        //printf("%s",token);
+        
         while (token){
             printf("%s ", token);
-            token = strtok(NULL, ",");
+            token = strtok(NULL, ","); // Continue
         }
-        printf("\n");
     }
 
+    printf("\n");
     fclose(fp);
+
 }
