39c69f71cc514327c650df0b77e5cbbcdfc4ac22
paekung <supakit.3t@gmail.com>
2025-09-12 22:37:16 +0700

    added struct data saving

--- main.c
+++ main.c
@@ -1,39 +1,70 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char ProductID[20];
+    char ProductName[100];
+    int Quantity;
+    int UnitPrice;
+} Product;
+
+Product products[100];
+int product_count = 0;
 
 void load_csv(const char *filename);
 
 int main(){
     load_csv("products.csv");
+    for(int i=0; i<product_count; i++){
+        printf("ProductID: %s, ProductName: %s, Quantity: %d, UnitPrice: %d\n", products[i].ProductID, products[i].ProductName, products[i].Quantity, products[i].UnitPrice);
+    }
     return 0;
 }
-
 
 void load_csv(const char *filename){
     FILE *fp;
     char line[1024];
+    
 
+    // Check if file opens successfully
     if(!(fp = fopen(filename, "r"))){
         perror("fopen");
         return;
     }
 
+    // Skip the header line
+    fgets(line, sizeof(line), fp);
+
     while(fgets(line, sizeof(line), fp)){
-        // printf("%s",line);
-
-        line[strcspn(line, "\r\n")] = '\0';
-
-        char *token = strtok(line, ",");
-        //printf("%s",token);
+        line[strcspn(line, "\r\n")] = '\0'; // Remove newline characters
+        char *token = strtok(line, ","); // Tokenize by comma
         
+        int index = 0;
         while (token){
-            printf("%s ", token);
-            token = strtok(NULL, ","); // Continue
+            switch(index){
+                case 0:
+                    strcpy(products[product_count].ProductID, token);
+                    break;
+                case 1:
+                    strcpy(products[product_count].ProductName, token);
+                    break;
+                case 2:
+                    products[product_count].UnitPrice = atof(token);
+                    break;
+                case 3:
+                    products[product_count].Quantity = atoi(token);
+                    break;
+                default:
+                    break;
+            }
+
+            token = strtok(NULL, ",");
+            index++;
         }
 
-        printf("\n");
+        product_count++;
     }
-
+    
     fclose(fp);
-
 }
