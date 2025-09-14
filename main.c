79c16e24a54678f0e3adb1df16606c2c3aef6a78
paekung <supakit.3t@gmail.com>
2025-09-14 20:35:14 +0700

    added add function

--- main.c
+++ main.c
@@ -13,10 +13,15 @@ Product *products = NULL;
 int product_count = 0;
 int product_capacity = 0;
 
-void load_csv(const char *filename);
+int load_csv(const char *filename);
+int add_product(const char *ProductID, const char *ProductName, int Quantity, int UnitPrice);
 
 int main(){
-    load_csv("products.csv");
+    if(load_csv("products.csv")){
+        printf("Failed to load CSV file.\n");
+        return 1;
+    };
+
     for(int i=0; i<product_count; i++){
         printf("ProductID: %s, ProductName: %s, Quantity: %d, UnitPrice: %d\n", products[i].ProductID, products[i].ProductName, products[i].Quantity, products[i].UnitPrice);
     }
@@ -25,15 +30,14 @@ int main(){
     return 0;
 }
 
-void load_csv(const char *filename){
+int load_csv(const char *filename){
     FILE *fp;
     char line[1024];
     
-
     // Check if file opens successfully
     if(!(fp = fopen(filename, "r"))){
         perror("fopen");
-        return;
+        return 1;
     }
 
     // Skip the header line
@@ -41,13 +45,14 @@ void load_csv(const char *filename){
 
     while(fgets(line, sizeof(line), fp)){
 
+        // Dynamically allocate or reallocate memory for products
         if (product_count == product_capacity) {
             product_capacity = product_capacity == 0 ? 10 : product_capacity * 2;
             products = realloc(products, product_capacity * sizeof(Product));
             if (!products) {
                 perror("realloc");
                 fclose(fp);
-                exit(1);
+                return 1;
             }
         }
 
@@ -81,4 +86,24 @@ void load_csv(const char *filename){
     }
 
     fclose(fp);
+    return 0;
+}
+
+int add_product(const char *ProductID, const char *ProductName, int Quantity, int UnitPrice){
+
+    if (product_count == product_capacity) {
+        product_capacity = product_capacity == 0 ? 10 : product_capacity * 2;
+        products = realloc(products, product_capacity * sizeof(Product));
+        if (!products) {
+            perror("realloc");
+            return 1;
+        }
+    }
+
+    strcpy(products[product_count].ProductID, ProductID);
+    strcpy(products[product_count].ProductName, ProductName);
+    products[product_count].Quantity = Quantity;
+    products[product_count].UnitPrice = UnitPrice;
+    product_count++;
+    return 0;
 }
