57bf8fc4b41ca403b4dae02b9428491676941aeb
paekung <supakit.3t@gmail.com>
2025-10-03 01:47:07 +0700

    add color

--- unittest.c
+++ unittest.c
@@ -799,18 +799,14 @@ int run_unit_tests(void) {
 
     printf("Running unit tests...\n\n");
 
-    const char *const pass_color = "\x1b[32m";
-    const char *const fail_color = "\x1b[31m";
-    const char *const reset_color = "\x1b[0m";
-
     for (size_t i = 0; i < total_tests; i++) {
         reset_test_environment();
         int rc = tests[i].func();
         if (rc == 0) {
-            printf("%s[PASS]%s %s\n", pass_color, reset_color, tests[i].name);
+            printf("\033[32m[PASS]\033[0m %s\n", tests[i].name);
             passed_tests++;
         } else {
-            printf("%s[FAIL]%s %s\n", fail_color, reset_color, tests[i].name);
+            printf("\033[31m[FAIL]\033[0m %s\n", tests[i].name);
         }
     }
 
